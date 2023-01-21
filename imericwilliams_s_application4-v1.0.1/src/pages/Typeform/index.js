c284ab9819e02ba444c400f33515fceff5296d99
adaora-orakwue <aorakwue2@gmail.com>
2023-01-21 18:00:32 -0500

    typeform embedded

--- imericwilliams_s_application4-v1.0.1/src/pages/Typeform/index.js
+++ imericwilliams_s_application4-v1.0.1/src/pages/Typeform/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { PopupButton } from '@typeform/embed-react'
+
 
 const TypeformPage = () => {
   return (
-    <>
-      <div className="bg-white_A700 h-[1024px] mx-[auto] w-[100%]"></div>
-    </>
+    <PopupButton id= "lUln6ei0" style={{ fontSize: 20 }} className="my-button">
+    click to open form in popup
+  </PopupButton>
   );
 };
 
